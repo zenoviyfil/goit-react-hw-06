fbb3996967393df872706d4f1f09bdcd379a2b2e
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-28 22:28:06 +0100

    init data organization

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,8 @@ import { useEffect, useState } from 'react'
 import ContactForm from './Components/ContactForm/ContactForm'
 import SearchBox from './Components/SearchBox/SearchBox'
 import ContactList from './Components/ContactList/ContactList'
+import contactsData from './contactsData.json'
 
-const contactsData = [
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-];
 
 function App() {
 
